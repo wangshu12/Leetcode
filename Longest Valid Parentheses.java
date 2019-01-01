fcd08008b0bc8e28142fd6324264534dc625df17
Wangshu Luo <wangshu.luo@stonybrook.edu>
2019-01-01 01:42:58 -0500

    DP || Stack

--- Longest Valid Parentheses.java
+++ Longest Valid Parentheses.java
@@ -29,3 +29,33 @@ class Solution {
         return Math.max(ret, right);
     }
 }
+//dp
+class Solution {
+    public int longestValidParentheses(String s) {
+        if(s == null) return 0;
+        if(s.length() == 0) return 0;
+        int[] dp = new int[s.length()];
+        int ret = 0;
+        dp[0] = 0;
+        for(int i = 1; i < s.length(); i++){
+            char current = s.charAt(i);
+            if(current == ')'){
+                if(s.charAt(i) == '('){
+                    dp[i] = 2;
+                    if(i - 2 >= 0){
+                        dp[i] += dp[i - 2];
+                    }
+                }else{
+                    if(i - 1 >= dp[i - 1] && s.charAt(i - dp[i - 1] - 1) == '('){
+                        dp[i] = dp[i-1]+2;
+                        if(i - 2 >= dp[i - 1]){
+                            dp[i] += dp[i - 2 - dp[i - 1]];//!!!!!!!
+                        }
+                    }
+                }
+            }
+            ret = Math.max(ret, dp[i]);
+        }
+        return ret;
+    }
+}
