8842ff06c48b89db0ad51c81c9b60389fa537b23
Wangshu Luo <wangshu.luo@stonybrook.edu>
2019-01-02 19:12:00 -0500

    Maximum

---
+++ Jump Game.java
@@ -0,0 +1,12 @@
+// we can use a integer variable to remember farthest index we can get.
+//
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int idx = 0; idx < nums.length; idx ++) {
+            if(idx == 0 || max >= idx) max = Math.max(max, idx + nums[idx]);
+            else return false;
+        }
+        return max >= nums.length - 1;
+    } 
+}
