8b129240b0d64211e471124c12636ce359ec21e9
Wangshu Luo <wangshu.luo@stonybrook.edu>
2019-01-08 17:56:20 -0500

    One fast pointer, one slow pointer and cycle in LinkedList

--- Find the Duplicate Number.java
+++ Find the Duplicate Number.java
@@ -1,35 +1,17 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        if(nums == null || nums.length == 0 || nums.length == 1) return -1;
-        int slow = nums[0];
-        int fast = nums[nums[0]];
-        
-        while(slow != fast){
+        if(nums == null || nums.length <= 1) return -1;
+        int slow = nums[0]; // be careful about the initialization
+        int fast = nums[nums[0]]; // be careful about the initialization
+        while(nums[slow] != nums[fast]){
             slow = nums[slow];
             fast = nums[nums[fast]];
         }
         slow = 0;
-        while(slow != fast){
+        while(nums[slow] != nums[fast]){
             slow = nums[slow];
             fast = nums[fast];
         }
-        return slow;
-    }
-}
-
-//binary search 
-class Solution {
-    public int findDuplicate(int[] nums) {
-        int low = 0, high = nums.length - 1;
-        while (low <= high) {
-            int mid = (int) (low + (high - low) * 0.5);
-            int cnt = 0;
-            for (int a : nums) {
-                if (a <= mid) ++cnt;
-            }
-            if (cnt <= mid) low = mid + 1;
-            else high = mid - 1;
-        }
-        return low;
+        return nums[slow];
     }
 }
