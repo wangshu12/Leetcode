ef94ca24cfb7e26b59a70953efc2173af8195860
Wangshu Luo <wangshu.luo@stonybrook.edu>
2019-03-31 18:29:07 -0400

    Math and array

--- Multiply Strings.java
+++ Multiply Strings.java
@@ -1,65 +1,36 @@
 class Solution {
     public String multiply(String num1, String num2) {
         if(num1.equals("0") || num2.equals("0")) return "0";
-        StringBuilder sb = new StringBuilder();
-        int index1 = num1.length() - 1;
-        int index2 = num2.length() - 1;
-        int end1 = 0;
-        int end2 = 0;
-        long carry_on = 0;
-        int zero_counter = 0;
-        int index = 0;
-        if(num1.charAt(0) == '-'){
-            end1 = 1;
-        }
-        if(num2.charAt(0) == '-'){
-            end2 = 1;
-        }
-        ArrayList<String> nums = new ArrayList<>();
-        while(index1 >= end1){
-            int c1 = (int)(num1.charAt(index1) - '0');
-            carry_on = 0;
-            index2 = 0;
-            sb.setLength(0);
-            for(int i = 0; i < zero_counter; i++){
-                sb.append("0");
-            }
-            while(index2 >= end2){
-                int c2 = (int)(num2.charAt(index2) - '0');
-                long result = c1 * c2 + carry_on;
+        
+        int[] table = new int[num1.length() * num2.length()+1];
+        int index = table.length - 1;
+        int end = table.length - 1;
+        //build table
+        for(int i = num1.length() - 1; i >= 0; i--){
+            int digit1 = (int)(num1.charAt(i) - '0');
+            int carry_on = 0;
+            int index_cursor = index;
+            int result = 0;
+            for(int j = num2.length() - 1; j >= 0; j--){
+                int digit2 = (int)(num2.charAt(j) - '0');
+                result = digit1 * digit2 + carry_on + table[index_cursor];
+                table[index_cursor] = result % 10;
                 carry_on = result / 10;
-                sb.append(Integer.toString((int)(result % 10)));
-                index2--;
+                index_cursor--;
             }
-            
             while(carry_on > 0){
-                sb.append(Integer.toString((int)(carry_on % 10)));
-                carry_on /= 10;
+                result = carry_on + table[index_cursor];
+                table[index_cursor] = result % 10;
+                carry_on = result / 10;
+                index_cursor--;
             }
-            String s = sb.reverse().toString();
-            index = Math.max(s.length(), index);
-            nums.add(s);
-            zero_counter++;
+            end = Math.min(index_cursor, end);
+            index--;
         }
-        carry_on = 0;
-        sb.setLength(0);
-        for(int i = 0; i < index; i++){
-            int sum = 0;
-            for(int j = 0; j < nums.size(); j++){
-                String num_str = nums.get(j);
-                sum += (int)(num_str.charAt(num_str.length() - i - 1) - '0');
-            }
-            sum += carry_on;
-            sb.append(Integer.toString(sum % 10));
-            carry_on = sum / 10;
+        StringBuilder sb = new StringBuilder();
+        for(int i = end + 1; i < table.length; i++){
+            sb.append(Integer.toString(table[i]));
         }
-        
-        while(carry_on > 0){
-            sb.append(carry_on % 10);
-            carry_on /= 10;
-        }
-        
-        return sb.reverse().toString();
+        return sb.toString();
     }
-
 }
