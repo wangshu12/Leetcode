5c7390ed0a2013fe0ada86f25f725b289c029e1e
Wangshu Luo <wangshu.luo@stonybrook.edu>
2019-04-07 16:48:56 -0400

    Math

---
+++ Count Primes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n <= 1) return 0;
+        int num = 2;
+        int counter = 0;
+        while(num < n){
+            if(isPrime(num)){
+                counter++;
+            }
+            num++;
+        }
+        return counter;
+    }
+    
+    private boolean isPrime(int num){
+        if(num == 2 || num == 3) return true;
+        if(num % 2 == 0 || num % 3 == 0) return false;
+        for(int i = 5; i * i <= num; i++){
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+}
